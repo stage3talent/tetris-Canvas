a7fb54585217535e3fb07c226a786ddbfa3a5c6d
drakemain <demain1990@gmail.com>
2017-11-10 06:54:03 -0800

    serve static tetris files

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const tetris = express();
+
+tetris.get('/', (req, res, next) => {
+  console.log('Tetris request.');
+  next();
+});
+
+tetris.use(express.static('static'));
+
+tetris.listen(3000);
