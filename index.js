9f887fed14f54121544cfa8b1732e892f8bc4d4b
Jim Bledsoe <jim@acceleration.net>
2022-04-22 14:45:33 -0400

    Replace Mongodb storage with localStorage emulator to store scores on server filesystem Fixes stage3talent/tetris-Canvas#1

--- index.js
+++ index.js
@@ -1,54 +1,63 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const mongo       = require('mongodb');
-const monk        = require('monk');
+const listenPort  = process.env.LISTENPORT || 3003;
+const storagePath = process.env.STORAGEPATH || "./TetrisScores";
+const storage     = require('node-persist');
+storage.initSync({
+  dir: storagePath
+});
 
-const db = monk('localhost:27017/TetrisScores');
 const tetris = express();
 tetris.use(bodyParser.json());
 tetris.use(bodyParser.urlencoded({ extended: true }));
 
 tetris.get('/', (req, res, next) => {
-  console.log('Tetris request.');
+  console.log('Tetris request recieved.');
   next();
 });
 
 tetris.get('/scores', (req, res) => {
   console.log('Fetching scores.');
 
-  getScoreboard().then(scoresArr => {
-    res.status(200).send(scoresArr);
-  });
+  let scoresArr = getScoreboard();
+  res.status(200).send(scoresArr);
 });
 
 tetris.post('/scores', (req, res) => {
-  console.log('Submitting new score');
+  console.log('Submitting new score...');
   if ((req.body.player && req.body.score)
   && (typeof(req.body.player) === 'string')
   && (req.body.player.length !== 0)
   && (!isNaN(req.body.score))) {
-    console.log('creating new scoreboard entry.');
     var entry = {
       'player': req.body.player,
       'score': Number(req.body.score),
       'date': new Date().getTime()
     }
-
-    db.get('usercollection').insert(entry, (err, result) => {
-      res.redirect('/');
-    }); 
+    setScoreboard(entry); 
   } else {
-    console.log('bad scoreboard submission.');
-    res.redirect('/');
+    console.log('  Bad scoreboard submission.');
   }
+  res.redirect('/');
 });
 
 tetris.use(express.static('static'));
-
-tetris.listen(3003);
+console.log("Listening on https://localhost:" + listenPort);
+tetris.listen(listenPort);
 
 function getScoreboard() {
-  return db.get('usercollection').find({}, {sort: {'score': -1}}, (e, docs) => {
-    return docs;
-  });
+  return storage.values();
+}
+
+function setScoreboard(entry) {
+  const old = storage.getItemSync(entry.player);
+  if (typeof old === 'undefined') {
+    storage.setItemSync(entry.player, entry);
+    console.log("  Score saved for new player", entry.player, entry.score);
+  } else if (entry.score > old.score) {
+    storage.setItemSync(entry.player, entry);
+    console.log("  Higher score saved for player", entry.player, entry.score, old.score);
+  } else {
+    console.log('  Skipping - User did not beat previous score', entry.player, entry.score)
+  }
 }
